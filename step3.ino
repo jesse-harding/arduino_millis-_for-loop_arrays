b8bd7215a542659155f9d035ecd44a56ff807e57
Jesse Harding <jesse.alexander.harding@gmail.com>
2020-03-01 20:03:06 -0500

    Update step3.ino

--- step3.ino
+++ step3.ino
@@ -1,3 +1,4 @@
+//for reference: https://www.arduino.cc/en/Reference.Int & https://www.arduino.cc/reference/en/language/variables/data-types/unsignedlong/
 //LETS CHANGE THAT "int" TO AN "unsigned long" DATATYPE on line 12
 //unsigned long variables can range from 0 to 4,294,967,295 whereas -32,768 to 32,767
 //now the timer will be able to run for days, as opposed to seconds without running out of room as we continue to update the value of prevMillis to the current time (in milliseconds from the start of the program)
